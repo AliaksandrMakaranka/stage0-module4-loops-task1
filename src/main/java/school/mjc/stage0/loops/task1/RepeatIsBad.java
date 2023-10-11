850d924363e701ba7840de46bfa5a04b33712946
AliaksandrMakaranka <aliaksandrmakaranka@gmail.com>
2023-10-11 22:49:38 +0200

    soat x20

--- src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
+++ src/main/java/school/mjc/stage0/loops/task1/RepeatIsBad.java
@@ -4,6 +4,26 @@ public class RepeatIsBad {
 
     @SuppressWarnings("java:S106")
     public void repeatIsBad(){
-
+        String msg = "writing the same code doesn't have much impact, and it's also time consuming";
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);
+        System.out.println(msg);
     }
 }
